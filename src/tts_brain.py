e2bd219936ad26185964928633af46ffcb8a10aa
carlosjdd <carlosjdd@users.noreply.github.com>
2020-10-28 08:22:36 +0100

    try to fix naming

--- src/tts_brain.py
+++ src/tts_brain.py
@@ -161,7 +161,7 @@ class brain():
 
         elif type[0] == 4:
             if type[1] <= 0 or type[1] > len(self.phrases[type[0]-1]):                                                                                          # If the type[1] is 0, or a wrong number, a random phrase is said.
-                self.tts_msg.data_string = str(text) + " " + self.phrases[type[0]+5][random.randint(0,len(self.phrases[type[0]+5])-1)]
+                self.tts_msg.data_string = text + " " + self.phrases[type[0]+5][random.randint(0,len(self.phrases[type[0]+5])-1)]
             else:                                                                                                                                               #Otherwise, it is said the phrase indicated in the type[1]
                 self.tts_msg.data_string = str(text) + " " + self.phrases[type[0]+5][type[1]-1]
 
