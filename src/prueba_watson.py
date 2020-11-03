82f5a2ab319f5d47b18d979c5ce6d6da54f99851
carlosjdd <carlosjdd@users.noreply.github.com>
2020-11-03 15:41:25 +0100

    Test spanish voice

--- src/prueba_watson.py
+++ src/prueba_watson.py
@@ -1,4 +1,4 @@
 from tts_watson.TtsWatson import TtsWatson
 
-TtsWatson = TtsWatson("carlycharly@hotmail.com","3pj58q.L", "en-US_AllisonVoice")
-TtsWatson.play("Hello World")
+TtsWatson = TtsWatson("carlycharly@hotmail.com","3pj58q.L", "es-ES_LauraVoice")
+TtsWatson.play("Hola mundooooo")
