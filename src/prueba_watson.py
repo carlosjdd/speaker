5bb5244b2a8db1253373944e2aac7589e59dd68b
carlosjdd <carlosjdd@users.noreply.github.com>
2020-11-04 09:08:09 +0100

    Change username with credentials

--- src/prueba_watson.py
+++ src/prueba_watson.py
@@ -7,6 +7,6 @@
 #voice.play("Hola mundooooo")
 from watson_text_talker import *
 
-text_talker = TextTalker(username='carlycharly@hotmail.com', password='3pj58q.L')
+text_talker = TextTalker('BjHfv8ct-Rypjy23mYp8q1pF74kHBEEwa0JBkP4tR2ne', 'https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/d383fac8-02d5-4896-b8d0-373e50273e1c')
 
 text_talker.say("Hello world!")
