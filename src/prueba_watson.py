ad401de84cc4d35cc32eb1e1ca4ee7d5d766d231
carlosjdd <carlosjdd@users.noreply.github.com>
2020-11-04 08:50:46 +0100

    Try different library for watson tts

--- src/prueba_watson.py
+++ src/prueba_watson.py
@@ -1,4 +1,12 @@
-from tts_watson.TtsWatson import TtsWatson
+#from tts_watson.TtsWatson import TtsWatson
+#import time
 
-TtsWatson = TtsWatson("carlycharly@hotmail.com","3pj58q.L", "es-ES_LauraVoice")
-TtsWatson.play("Hola mundooooo")
+#voice = TtsWatson("carlycharly@hotmail.com","3pj58q.L", "es-ES_LauraVoice")
+#print("credenciales generadas")
+#time.sleep(5)
+#voice.play("Hola mundooooo")
+from watson_text_talker import *
+
+text_talker = TextTalker(username='carlycharly@hotmail.com', password='3pj58q.L')
+
+text_talker.say("Hello world!")
