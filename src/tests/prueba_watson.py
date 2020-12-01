c441916e17e777c106a9a0e6d6eebe0de5f2ef1b
carlosjdd <carlosjdd@users.noreply.github.com>
2020-12-01 15:43:21 +0100

    Test IBM voice

--- src/tests/prueba_watson.py
+++ src/tests/prueba_watson.py
@@ -7,8 +7,8 @@
 #voice.play("Hola mundooooo")
 from watson_text_talker import *
 
-text_talker = TextTalker(api_key='BjHfv8ct-Rypjy23mYp8q1pF74kHBEEwa0JBkP4tR2ne') 
+text_talker = TextTalker(api_key='BjHfv8ct-Rypjy23mYp8q1pF74kHBEEwa0JBkP4tR2ne')
 
 #('https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/d383fac8-02d5-4896-b8d0-373e50273e1c')
 
-text_talker.say("Hola mundo, esto es una prueba de la voz de amazon llamada Laura. A ver que tal suena")
+text_talker.say("Hola mundo, esto es una prueba de la voz de IBM llamada Laura. A ver que tal suena")
